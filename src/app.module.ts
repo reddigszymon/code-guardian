125968c89a81afb12b43a54799c67c309f7085c3
reddigszymon <mixowo@hotmail.com>
2026-03-15 20:09:29 +0100

    fix: enable GraphQL playground in Docker (disable Apollo CSRF check)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,8 +13,10 @@ import { ScanModule } from './scan/scan.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'schema.gql'),
-      playground: process.env.NODE_ENV !== 'production',
-    }),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+      csrfPrevention: false,
+    } as ApolloDriverConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
